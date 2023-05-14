ccea9d5f19edc260d5ca37a6b89acd48ce8044b8
Lucendayo <Lucendayo@gmail.com>
2023-05-14 16:40:20 +0400

    committed

--- chessPuzzleGeneration.py
+++ chessPuzzleGeneration.py
@@ -1,28 +1,28 @@
-#import chess
-#import chess.engine
-#import os
-#import sys
+import chess
+import chess.engine
+import os
+import sys
 
-#arguments = sys.argv
-#pondertime = 3600
-#maxmoves = 100
-#gamecount = 1
+arguments = sys.argv
+pondertime = 3600
+maxmoves = 100
+gamecount = 1
 #here we assume the engine file is in same folder as our python script
-#path = os.getcwd()
-#os.chmod(path, 0o0111)
-#engine = chess.engine.SimpleEngine.popen_uci('Stockfish')
+path = os.getcwd()
+os.chmod(path, 0o0111)
+engine = chess.engine.SimpleEngine.popen_uci(path + '/' + 'Stockfish')
 
-#dictsidetomove = {True:'white',False:'black'}
-#\notationdict = {True:'.', False:'...'}
+dictsidetomove = {True:'white',False:'black'}
+notationdict = {True:'.', False:'...'}
 
-#for i in range(gamecount):
-    #board = chess.Board()
-    #while not board.is_game_over() and board.fullmove_number<=maxmoves:
-        #result = engine.play(board,chess.engine.Limit(time=pondertime))
-        #print(dictsidetomove[board.turn]+' played '+str(board.fullmove_number)+notationdict[board.turn]+str(board.san(result.move)))
-        #board.push(result.move)
-    #print('Iteration '+str(i+1)+'-----')
-    #print(board)
+for i in range(gamecount):
+    board = chess.Board()
+    while not board.is_game_over() and board.fullmove_number<=maxmoves:
+        result = engine.play(board,chess.engine.Limit(time=pondertime))
+        print(dictsidetomove[board.turn]+' played '+str(board.fullmove_number)+notationdict[board.turn]+str(board.san(result.move)))
+        board.push(result.move)
+    print('Iteration '+str(i+1)+'-----')
+    print(board)
     
-#engine.quit()
+engine.quit()
     
