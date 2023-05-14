898c0726557d31ea1e8d1cdf0d5c565319969ab0
Lucendayo <Lucendayo@gmail.com>
2023-05-14 18:20:24 +0400

    Added some generation and ascii display Need to add: OS Identify and modify

--- chessPuzzleGeneration.py
+++ chessPuzzleGeneration.py
@@ -1,28 +1,54 @@
 import chess
 import chess.engine
+import chess.svg
+from IPython.display import display, SVG
 import os
 import sys
+import platform
+
+def get_operating_system():
+    system = platform.system()
+    if system == "Windows":
+        return "Windows"
+    elif system == "Darwin":
+        return "macOS"
+    elif system == "Linux":
+        return "Linux"
+    else:
+        print("Error. Operating System not identified.")
+        quit()
+
+# Get the operating system and save to operatingSystem variable
+operatingSystem = get_operating_system()
 
 arguments = sys.argv
 pondertime = 3600
 maxmoves = 100
 gamecount = 1
-#here we assume the engine file is in same folder as our python script
 path = os.getcwd()
 os.chmod(path, 0o0111)
-engine = chess.engine.SimpleEngine.popen_uci(path + '/' + 'Stockfish')
+engine = chess.engine.SimpleEngine.popen_uci(os.path.join(path, "Stockfish"))
 
 dictsidetomove = {True:'white',False:'black'}
 notationdict = {True:'.', False:'...'}
+iterations = 1
+for i in range(iterations):
+    print("Iteration:", i+1)
 
-for i in range(gamecount):
+    # Create a new chess board for each iteration
     board = chess.Board()
-    while not board.is_game_over() and board.fullmove_number<=maxmoves:
-        result = engine.play(board,chess.engine.Limit(time=pondertime))
-        print(dictsidetomove[board.turn]+' played '+str(board.fullmove_number)+notationdict[board.turn]+str(board.san(result.move)))
+
+    while not board.is_game_over():
+        # Get the best move for the current position from the engine
+        result = engine.play(board, chess.engine.Limit(time=2.0))
+
+        # Print the move and apply it to the board
+        print("Move:", result.move)
         board.push(result.move)
-    print('Iteration '+str(i+1)+'-----')
-    print(board)
-    
-engine.quit()
-    
+
+        # Display the current chessboard
+        print(board)
+
+    print("Game over\n")
+
+# Close the engine
