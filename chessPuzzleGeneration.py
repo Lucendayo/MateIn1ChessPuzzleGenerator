e18a0b4098b6f672fcdd9e5a1badd8def2dfdf23
Lucendayo <Lucendayo@gmail.com>
2023-05-18 18:29:18 +0400

    Added MacOS support and Results Display

--- chessPuzzleGeneration.py
+++ chessPuzzleGeneration.py
@@ -26,17 +26,17 @@ path = os.getcwd()
 os.chmod(path, 0o0111)
 # Assigning different Stockfish version depending on the OperatingSystem variable
 if operatingSystem == "Windows":
-    engine = chess.engine.SimpleEngine.popen_uci(os.path.join(path, "Stockfish_Win"))
+    engine = chess.engine.SimpleEngine.popen_uci(os.path.join(path, "Stockfish"))
 elif operatingSystem == "macOS":
-    engine = chess.engine.SimpleEngine.popen_uci("/Applications/Stockfish.app")
+    engine = chess.engine.SimpleEngine.popen_uci("/opt/homebrew/Cellar/stockfish/15.1/bin/stockfish")
 else:
     print("Error, operatingSystem variable null")
 dictsidetomove = {True:'white',False:'black'}
 notationdict = {True:'.', False:'...'}
 iterations = 1
 
-engine.options[{"Skill Level" : 8}]
-
+engine.options["Skill Level"] = 8
+print("worked")
 for i in range(iterations):
     print("Iteration:", i+1)
 
@@ -45,8 +45,7 @@ for i in range(iterations):
 
     while not board.is_game_over():
         # Get the best move for the current position from the engine
-        result = engine.play(board, chess.engine.Limit
-        (time=2.0))
+        result = engine.play(board, chess.engine.Limit(time=1))
 
         # Print the move and apply it to the board
         print("Move:", result.move)
@@ -56,8 +55,25 @@ for i in range(iterations):
         # Display the current chessboard
         print(board)
 
-    print("Game over\n")
+        # Find end and its causes
+        if board.is_checkmate():
+            winner = dictsidetomove[board.turn]
+            print("Checkmate! {} wins. Game over.".format(winner))
+            engine.quit()
+            break
+        elif board.is_stalemate():
+            print("Stalemate! Game over.")
+            engine.quit()
+            break
+        elif board.is_repetition():
+            print("Repetition! Draw! Game over.")
+            engine.quit()
+            break
+        elif board.is_insufficient_material():
+            print("Insufficient material! Draw! Game over.")
+            engine.quit()
+            break
+        
 
-# Close the engine
-engine.quit()
+
 
